b1550e699619d36cb1af8f72eb0c4e3c28aa69d6
BekturKochorbaev <bekturkochorbaev64@gmail.com>
2024-11-28 12:18:22 +0600

    user

--- myproject/shop/models.py
+++ myproject/shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
+from django.core.validators import MinValueValidator,MaxValueValidator
 
-# Create your models here.
+
+class User(AbstractUser):
+    age = models.PositiveIntegerField(validators=[MaxValueValidator(90), MinValueValidator(10)])
+    phone_number = PhoneNumberField(null=True, blank=True, region='KG')
+
+    def __str__(self):
+        return self.first_name
